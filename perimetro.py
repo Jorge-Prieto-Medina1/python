ca548479333851d9f1d155ced54ff89a0cd03d0f
Jorge <jorgepri00@gmail.com>
2020-11-16 13:06:32 +0100

    Initial commit

---
+++ perimetro.py
@@ -0,0 +1,11 @@
+print("vamos ac calcular el perimetro y el area de un cuadrado")
+lado = int (input ("introduce el lado"))
+perimetro = lado+4
+area = lado**2
+print(area)
+
+print("vamos ac calcular el perimetro y el area de un cubo")
+lado = int (input ("introduce el lado"))
+perimetro = lado*6
+area = (lado**2)*6
+print(area," ", perimetro)
